52ad4f21e2215bebac4611f8b7a668f9bcba2778
Vinay Subramanian <vinay@Vijays-MacBook-Pro-2.local>
2019-01-04 08:24:16 -0800

    Redo

---
+++ src/com/iScienceCorp/tutoringSessions/TutoringSession.java
@@ -0,0 +1,73 @@
+package com.iScienceCorp.tutoringSessions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TutoringSession {
+
+	private Tutor tutor;
+	private ArrayList<Student> students = new ArrayList<>();
+	private String subject;
+	private String day;
+	private String time;
+	private int minutes;	
+	
+	public TutoringSession(String subject, String day, String time) {
+		this.subject = subject; 
+		this.day = day;
+		this.time = time;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "TutoringSession [tutor=" + tutor + ", students=" + students + ", subject=" + subject + ", day=" + day
+				+ ", time=" + time + ", minutes=" + minutes + "]";
+	}
+
+
+	public String getSubject() {
+		return subject;
+	}
+	
+	public String getDay() {
+		return day;
+	}
+	
+	public String getTime() {
+		return time;
+	}
+
+	public Tutor getTutor() {
+		return tutor;
+	}
+
+	public void setTutor(Tutor tutor) {
+		this.tutor = tutor;
+	}
+
+	public ArrayList getStudents() {
+		return students;
+	}
+
+	public void setStudents(ArrayList students) {
+		this.students = students;
+	}
+
+	public int getMinutes() {
+		return minutes;
+	}
+
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
+	
+	public void addStudent(Student student) {
+		students.add(student);
+	}
+	
+	public void removeStudent(Student student) {
+		students.remove(student);
+	}
+		
+}
